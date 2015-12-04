29053900bd85cc32bb273798d00136d19423c81c
maxcl730 <maxcl730@gmail.com>
2015-12-04 19:09:55 +0800

    add db lib

--- applog/db.py
+++ applog/db.py
@@ -1,2 +1,15 @@
 # coding=utf-8
 # __author__ = 'chengliang'
+import pymysql
+
+
+def db_conn():
+    conn = pymysql.connect(host='192.168.1.245', port=3306, user='ipad', passwd='Zaq1xsw@', db='app_action')
+    cur = conn.cursor()
+    cur.execute("desc app_user_behavior")
+    #cur.execute("insert")
+    #conn.commit()
+    for r in cur:
+        print(r)
+    cur.close()
+    conn.close()
--- applog_run.py
+++ applog_run.py
@@ -1,12 +1,12 @@
 # coding=utf-8
 # __author__ = 'chengliang'
 
-from applog import logfile, user_actions
+from applog import logfile, user_actions, db
 from pprint import pprint
 
 if __name__ == "__main__":
     LOG_PATH = '/Users/chengliang/Documents/work/dev/test/log'
-    #pprint(logfile.list_logfiles(LOG_PATH))
+    db.db_conn()
     for log_file in logfile.list_logfiles(LOG_PATH, '2015-11-27'):
         print(log_file)
         log_dict = logfile.load_logfile(log_file)
